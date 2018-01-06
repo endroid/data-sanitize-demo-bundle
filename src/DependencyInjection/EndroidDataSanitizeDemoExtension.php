1927a6a488fad34b32de62d53b3798c448c52b10
endroid <info@endroid.nl>
2018-01-06 12:12:58 +0000

    Remove unnecessary logic

--- src/DependencyInjection/EndroidDataSanitizeDemoExtension.php
+++ src/DependencyInjection/EndroidDataSanitizeDemoExtension.php
@@ -9,28 +9,13 @@
 
 namespace Endroid\DataSanitizeDemoBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
-class EndroidDataSanitizeDemoExtension extends Extension implements PrependExtensionInterface
+class EndroidDataSanitizeDemoExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container): void
     {
         // Do nothing
-    }
-
-    public function prepend(ContainerBuilder $container): void
-    {
-        // Add the package to the assets configuration so the correct manifest is used
-        $container->prependExtensionConfig('framework', [
-            'assets' => [
-                'packages' => [
-                    'endroid_data_sanitize_demo' => [
-                        'json_manifest_path' => '%kernel.project_dir%/public/bundles/endroiddatasanitizedemo/build/manifest.json',
-                    ],
-                ],
-            ],
-        ]);
     }
 }
